f0fa797ed8fca3de40369df599e691d8cce0bca2
pedro-mjgt <pedro.mjgt@gmail.com>
2024-10-12 00:05:25 -0500

    gateway terminado

---
+++ src/test/java/pe/edu/cibertec/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
